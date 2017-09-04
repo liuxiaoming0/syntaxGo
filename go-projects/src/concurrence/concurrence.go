524d8336e1a970658a7c0e03eb7e62520af5b902
danae <982609580@qq.com>
2017-09-04 20:14:48 +0800

    test channel

--- go-projects/src/concurrence/concurrence.go
+++ go-projects/src/concurrence/concurrence.go
@@ -1,33 +1,58 @@
 package concurrence
 
 import(
-	"fmt"
+	"sync"
+	"errors"
 )
 
-type myQueue interface{
-	put() error
-	get() error
-}
+var data chan Info = make(chan Info, 1024);
+var queueLock sync.Mutex;
+var num int = 0;
 
 type Info struct{
-	len  int
-	head string
-	body string
+	Len  int
+	Head string
+	Body string
+}
+
+type myQueue interface{
+	Put() error
+	Get(info Info) error
+	GetNum() int;
+	CloseChan()bool;
 }
 
 
-var Data chan Info = make(chan Info, 1024);
-
-func (info Info) put() error{
-	Data<-info;
-	defer func() {
-		if err := recover();err != nil{
-			fmt.Println(err);
-		}
-	}()
+func (info Info) Put() (err error){
+	if info.Len == 0 {
+		return errors.New("put info len is null");
+	}
+	queueLock.Lock();
+	data<-info;
+	num++;
+	queueLock.Unlock();
+	return nil;
 }
 
 
-func (info Info) get() error{
-	
+func (info Info) Get() (err error){
+	queueLock.Lock();
+	info = <-data;
+	num--;
+	queueLock.Unlock();
+	if info.Len < 0 {
+		return errors.New("get info len < 0");
+	}
+	return nil;
+}
+
+func (info Info) GetNum() int{
+	return num;
+}
+
+//channel closing principe
+func (info Info)CloseChan() bool{
+	close(data);
+	_, ok := <-data;
+	return ok;
 }
--- go-projects/src/main.go
+++ go-projects/src/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "basic"
+import(
+	//"basic"
+	"concurrence"
+	"fmt"
+	"runtime"
+	"time"
+) 
 
+
+func testChannel(){
+
+	var info concurrence.Info = concurrence.Info{Len:8, Head:"TCP", Body:"im ok"};
+	for i:=0; i<1000000; i++ {
+		go info.Put();
+	}
+	/*
+	num := info.GetNum();
+	fmt.Println("info in Queue num is:", num);
+	info.Get();
+	fmt.Println("info->len:", info.Len,
+				"info->head:", info.Head,
+				"info->body", info.Body);
+
+	num = info.GetNum();
+	fmt.Println("info in Queue num is:", num);
+	*/
+}
 
 func main(){
 
@@ -13,7 +38,20 @@ func main(){
 
 	// basic.TestMap();
 
-	basic.TestInterface();
-	
+	//basic.TestInterface();
+
+	startTime := time.Now();
+
+	numCpu := runtime.NumCPU();
+	runtime.GOMAXPROCS(numCpu);
+	fmt.Println("CPU num:", numCpu);
+
+
+	testChannel();
+
+	elapsed := time.Since(startTime);
+
+	fmt.Println("put data to channel waste time:", elapsed);
+
 }
 
