38dd53d858ddf2870f3f4eb700a4c265d44f75e4
danae <982609580@qq.com>
2017-09-05 20:36:47 +0800

    test go concurrence

--- go-projects/src/concurrence/concurrence.go
+++ go-projects/src/concurrence/concurrence.go
@@ -3,9 +3,11 @@ package concurrence
 import(
 	"sync"
 	"errors"
+	"time"
 )
 
-var data chan Info = make(chan Info, 1024);
+//尽量不要使用这种共享数据，因为需要加锁费时，要用channel代替
+var data chan Info = make(chan Info, 8*10000);
 var queueLock sync.Mutex;
 var num int = 0;
 
@@ -27,22 +29,24 @@ func (info Info) Put() (err error){
 	if info.Len == 0 {
 		return errors.New("put info len is null");
 	}
-	queueLock.Lock();
 	data<-info;
+	queueLock.Lock();
 	num++;
 	queueLock.Unlock();
+	time.Sleep(1000);
 	return nil;
 }
 
 
 func (info Info) Get() (err error){
-	queueLock.Lock();
 	info = <-data;
+	queueLock.Lock();
 	num--;
 	queueLock.Unlock();
 	if info.Len < 0 {
 		return errors.New("get info len < 0");
 	}
+
 	return nil;
 }
 
--- go-projects/src/main.go
+++ go-projects/src/main.go
@@ -11,10 +11,20 @@ import(
 
 func testChannel(){
 
+	startTime := time.Now();
+	
+	numCpu := runtime.NumCPU();
+	runtime.GOMAXPROCS(numCpu);
+	fmt.Println("CPU num:", numCpu);
+	
 	var info concurrence.Info = concurrence.Info{Len:8, Head:"TCP", Body:"im ok"};
-	for i:=0; i<1000000; i++ {
+	for i:=0; i<1000; i++ {
 		go info.Put();
 	}
+
+	//其实这个时间这样统计也是错误的	
+	elapsed := time.Since(startTime);	
+	fmt.Println("put data to channel waste time:", elapsed);
 	/*
 	num := info.GetNum();
 	fmt.Println("info in Queue num is:", num);
@@ -22,14 +32,17 @@ func testChannel(){
 	fmt.Println("info->len:", info.Len,
 				"info->head:", info.Head,
 				"info->body", info.Body);
-
-	num = info.GetNum();
-	fmt.Println("info in Queue num is:", num);
 	*/
+	//保证上面的协程都处理完了，go没有wait这种等待线程处理完的函数，需要自己管理
+	time.Sleep(1000*1000*1000);
+
+	num := info.GetNum();
+	fmt.Println("info in Queue num is:", num);
+	
 }
 
-func main(){
 
+func testSyntax(){
 	// basic.TestVar();
 
 	// basic.TestString();
@@ -40,18 +53,13 @@ func main(){
 
 	//basic.TestInterface();
 
-	startTime := time.Now();
+	//testChannel();
+}
 
-	numCpu := runtime.NumCPU();
-	runtime.GOMAXPROCS(numCpu);
-	fmt.Println("CPU num:", numCpu);
+func main(){
 
 
-	testChannel();
 
-	elapsed := time.Since(startTime);
-
-	fmt.Println("put data to channel waste time:", elapsed);
 
 }
 
